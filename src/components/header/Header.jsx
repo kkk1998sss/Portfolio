6edf0fc8ebe61ae28c7871996b5ea4a287906918
kundan Sharma <42888188+kkk1998sss@users.noreply.github.com>
2025-05-27 09:02:37 +0530

    Update fonts and name styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Kundan</h1>
+        <h1 className="name-animation">Kundan Sharma</h1>
         <h5 className="text-light">Full Stack Developer</h5>
         <CTA />
         <HeaderSocial />
